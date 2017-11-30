79f2a388736dee52ffc5e4f2ee77244cce6d31bb
Alan Jenkins <alan.jenkins@claranet.uk>
2017-11-30 07:13:19 +0000

    Make it so config_files fact doesn't fail if wpa_supplicant dir is missing.

--- lib/facter/wpa_supplicant_config_exists.rb
+++ lib/facter/wpa_supplicant_config_exists.rb
@@ -1,12 +1,17 @@
 Facter.add(:wpa_supplicant) do
   setcode do
-		facts = {}
-		facts['config_files'] = []
+    wpa_supplicant_dir = '/etc/wpa_supplicant'
+    facts = {}
+    facts['config_files'] = []
 
-		Dir.foreach('/etc/wpa_supplicant') do |file|
-      next if file == '.' or file == '..'
-			facts['config_files'] << file
-		end
+    if File.directory?(wpa_supplicant_dir)
+      Dir.foreach(wpa_supplicant_dir) do |file|
+        next if file == '.' or file == '..'
+        facts['config_files'] << file
+      end
+    else
+      facts['config_files'] = []
+    end
 
     facts
   end
